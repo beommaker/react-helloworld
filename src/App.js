6ce1e04d6246c0244b54fd963591d10f96b3b1c5
beommaker <beom.maker@gmail.com>
2017-11-21 16:52:42 +0900

    [study] Reactjs.org
    Components and Props / setInterval / Components and Props

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>
-          I love you, {formatName(user)}
-        </h1>
-        {getGreeting(user)}
-        {getGreeting()}
+      <div className="APP">
+        <Welcome name="Daji" />
+        <Welcome name="Beom" />
+        <Welcome name="Sam" />
       </div>
     );
   }
 }
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+function Welcome(props){
+  return <h1> Hello, {props.name}</h1>;
 }
 
-const user = {
-  firstName: 'Daji',
-  lastName: 'Lee',
-};
-
-function getGreeting(user) {
-  if (user) {
-    return <h1>I love you, {formatName(user)}!</h1>;
-  }
-  return <h1>Hello, Stranger</h1>;
-}
-
-const element = {
-  type: 'h1',
-  props: {
-    className: 'greeting',
-    children: 'Hello, world'
-  }
-};
 
 export default App;
